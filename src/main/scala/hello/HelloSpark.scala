f42a58eb41be760a6ba91ddad65d357fed59b099
Kristof Jozsa <kristof.jozsa@gmail.com>
2015-04-21 09:34:48 +0200

    first working version of transactions processing

--- src/main/scala/hello/HelloSpark.scala
+++ src/main/scala/hello/HelloSpark.scala
@@ -1,19 +1,50 @@
 package hello
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import SparkContext.rddToPairRDDFunctions
 
-object HelloSpark extends App {
-  val master = "local[2]"
-  val conf = new SparkConf().setAppName("hello").setMaster(master)
-  val sc = new SparkContext(conf)
+object HelloSpark {
+  def process(targetCurrency: String) = {
+    val sc = new SparkContext(new SparkConf().setAppName("challenge1").setMaster("local[2]"))
 
-  try {
-    val file = sc.textFile("build.sbt")
-    val counts: RDD[Int] = file.map(s => s.split("(\\s\\(\\))").length)
+    try {
+      val results = benchmark(parse(sc, targetCurrency))
+      println(results.toList)
 
-    println(counts.reduce(_ + _))
-  } finally {
-    sc.stop
+    } finally {
+      sc.stop
+    }
+  }
+
+  def parse(sc: SparkContext, targetCurrency: String): Array[(String, Double)] = {
+    val rates = sc.textFile("exchangerates.csv")
+      .map(_.split(","))
+      .filter { case Array(from, to, rate) => to == targetCurrency }
+      .map { case Array(from, to, rate) => (from, rate.toDouble) }
+      .collect()
+      .toMap
+
+    def exchange(currency: String, amount: String) = if (currency == targetCurrency) amount.toDouble else amount.toDouble * rates(currency)
+
+    sc.textFile("transactions.csv")
+      .map(_.split(","))
+      .map { case Array(partner, currency, amount) => (partner, exchange(currency, amount)) }
+      .reduceByKey(_ + _)
+      .collect()
+  }
+
+  def main(args: Array[String]) {
+    val targetCurrency = "GBP" // TODO parse args
+    process(targetCurrency)
+  }
+
+  def benchmark[T](block: => T): T = {
+    val start = System.currentTimeMillis()
+    try {
+      block
+    } finally {
+      val end = System.currentTimeMillis()
+      println("## executed in " + (end - start) + "ms")
+    }
   }
 }
